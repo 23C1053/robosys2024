7318a32eb5793d663a96973e766958731a0dd4c6
masaya <s23c1053ar@s.chibakoudai.jp>
2024-12-10 00:36:48 +0900

    1210

---
+++ test_cal.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2024 Masaya Kobayashi
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+    echo "${1}行目が違う"
+    res=1
+}
+
+res=0
+
+out=$(echo "1 60 160 19" | ./taisha_calc)
+a_r="1552.159"
+[ "${out}" = "${a_r}" ] || ng "$LINENO"
+ 
+
+out=$(echo "0 60 160 19" | ./taisha_calc)
+b_r="1415.823"
+[ "${out}" = "${b_r}" ] || ng "$LINENO"
+
+out=$(echo "1 2 3" | ./taisha_calc)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo "1 2 3 a" | ./taisha_calc)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo "" | ./taisha_calc)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo ok
+
+exit $res
